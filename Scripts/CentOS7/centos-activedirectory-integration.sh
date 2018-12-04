4f8ac42453b5b204ecb3047db2847aeb987b6fc6
Karl Mathias Moberg <karlmathias@gmail.com>
2018-12-04 08:45:07 +0100

    Update centos-activedirectory-integration.sh

--- Scripts/CentOS7/centos-activedirectory-integration.sh
+++ Scripts/CentOS7/centos-activedirectory-integration.sh
@@ -1,11 +1,23 @@
 navn=$HOSTNAME;
 navn2="ldap-test";
 
-
 if [[ $navn == $navn2 ]]
 then
-        echo "Hostname is invalid. Please enter a new hostname:"
+        echo "Hostname cannot be localhost. Please enter a new hostname:"
         read navn;
 
-        echo $navn;
+        hostnamectl set-hostname $navn;
 fi
+
+yum install sssd realmd oddjob oddjob-mkhomedir adcli samba-common samba-common-tools krb5-workstation openldap-clients policycoreutils-python -y
+realm list
+
+echo "Enter domain admin credentials"
+read domainUser
+
+echo "Enter domain name"
+read domainName
+
+echo "realm join --user=$domainUser $domainName"
+realm join --user=$domainUser $domainName
+
