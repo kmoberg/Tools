7ad5af413d6738040a13f9af31af74557afae39d
Karl Mathias Moberg <karlmathias@gmail.com>
2018-12-04 09:05:53 +0100

    Update centos-activedirectory-integration.sh
    Removed already working code to test only the renaming of files using curl.

--- Scripts/CentOS7/centos-activedirectory-integration.sh
+++ Scripts/CentOS7/centos-activedirectory-integration.sh
@@ -1,27 +1,6 @@
-navn=$HOSTNAME;
-navn2="localhost";
-
-if [[ $navn == $navn2 ]]
-then
-        echo "Hostname cannot be localhost. Please enter a new hostname:"
-        read navn;
-
-        hostnamectl set-hostname $navn;
-fi
-
-yum install sssd realmd oddjob oddjob-mkhomedir adcli samba-common samba-common-tools krb5-workstation openldap-clients policycoreutils-python -y
-realm list
-
-echo "Enter domain admin credentials"
-read domainUser
-
-echo "Enter domain name"
-read domainName
-
-echo "realm join --user=$domainUser $domainName"
-realm join --user=$domainUser $domainName
-
 sed -i 's/services = nss, pam/services = nss, pam, ssh/g' /etc/sssd/sssd.conf.bak
-echo "#Active Directory Integration" >> /etc/ssh/sshd_config
-echo "AllowGroups Linux\ Admin linux-user" >> /etc/ssh/sshd_config
-echo "%Linux\ Admin 	ALL=(ALL)	ALL" > /etc/sudoers.d/"Linux Admin"
+sed -i 's/use_fully_qualitied_names = True/use_fully_qualitied_names = false/g' /etc/sssd/sssd.conf.bak
+sed -i 's/%u%d/%u/g' /etc/sssd/sssd.conf.bak
+echo "#Active Directory Integration" >> /etc/ssh/sshd_config.bak
+echo "AllowGroups Linux\ Admin linux-user" >> /etc/ssh/sshd_config.bak
+echo "%Linux\ Admin 	ALL=(ALL)	ALL" > /etc/sudoers.d/"Linux Admin Test"
