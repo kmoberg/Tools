df30a75426aa938f58938cd40aac3f60b34538d5
Karl Mathias Moberg <karlmathias@gmail.com>
2018-12-04 08:11:16 +0100

    Update centos-activedirectory-integration.sh

--- Scripts/CentOS7/centos-activedirectory-integration.sh
+++ Scripts/CentOS7/centos-activedirectory-integration.sh
@@ -1,2 +1,11 @@
-hostname= $HOSTNAME;
-echo $hostname;
+navn=$HOSTNAME;
+navn2="ldap-test";
+
+
+if [[ $navn == $navn2 ]]
+then
+        echo "Hostname is invalid. Please enter a new hostname:"
+        read navn;
+
+        echo $navn;
+fi
