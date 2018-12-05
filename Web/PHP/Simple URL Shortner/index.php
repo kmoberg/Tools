19525a7896705d3efe4e842a7727baa1d7d7e797
Karl Mathias Moberg <karlmathias@gmail.com>
2018-12-05 15:26:49 +0100

    Added an error.html

--- Web/PHP/Simple URL Shortner/index.php
+++ Web/PHP/Simple URL Shortner/index.php
@@ -6,6 +6,5 @@ if(isset($_GET['l']) && array_key_exists($_GET['l'], $links)){
     header('Location: ' . $links[$_GET['l']]);
 }
 else{
-    header('HTTP/1.0 404 Not Found');
-    echo 'Unknown link.';
+	include("error.html");
 }
