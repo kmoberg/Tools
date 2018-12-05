a68c139d29b06f32c4989b9eafa5d1bf798620a9
Karl Mathias Moberg <karlmathias@gmail.com>
2018-12-05 14:24:34 +0100

    Update install-apache.sh

--- Scripts/CentOS7/install-apache.sh
+++ Scripts/CentOS7/install-apache.sh
@@ -5,20 +5,20 @@ fi
 
 
 
-yum update -y
+yum -y update
 
-yum install httpd -y
+yum -y install httpd
 systemctl start httpd
 systemctl enable httpd
 firewall-cmd --zone=public --permanent --add-service=http
 firewall-cmd --zone=public --permanent --add-service=https
 systemctl restart firewalld
 
-yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-yum install yum-utils
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum -y install yum-utils
 yum-config-manager --enable remi-php72
-yum install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo 
+yum -y install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo 
 
 setsebool -P httpd_can_network_connect
 
